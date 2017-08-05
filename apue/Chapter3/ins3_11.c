1d29277f07730bd5c50c9cd08f5255f0b1a4aab0
DevinKin <DevinKin@163.com>
2017-08-05 09:51:22 +0800

    ins3_11

---
+++ apue/Chapter3/ins3_11.c
@@ -0,0 +1,55 @@
+/*************************************************************************
+    > File Name: ins3_11.c
+    > Author: King
+    > Mail: arturiapendragon_1@163.com 
+    > Created Time: 2017年08月05日 星期六 08时56分15秒
+ ************************************************************************/
+
+#include <apue.h>
+#include <fcntl.h>
+
+int
+main(int argc, char *argv[])
+{
+	int		val;
+
+	if (argc != 2)
+		err_quit("usage: a.out <descriptor#>");
+
+	if ((val = fcntl(atoi(argv[1]), F_GETFL, 0)) < 0)
+		err_sys("fcntl error for fd %d", atoi(argv[1]));
+
+	switch (val & O_ACCMODE) {
+		case O_RDONLY:
+			printf("read only");
+			break;
+
+		case O_WRONLY:
+			printf("write only");
+			break;
+
+		case O_RDWR:
+			printf("write only");
+			break;
+
+		default:
+			err_dump("unknown access mode");
+	}
+
+	if (val & O_APPEND)
+		printf(", append");
+
+	if (val & O_NONBLOCK)
+		printf(", nonblocking");
+
+	if (val & O_SYNC)
+		printf(", synchronous writes");
+
+#if !defined(_POSIX_C_SOURCE) && defined(O_FSYNC) && (O_FSYNC != O_SYNC)
+	if (val & O_FSYNC)
+		printf(", synchronous writes");
+#endif
+	putchar('\n');
+	exit (0);
+}
+
