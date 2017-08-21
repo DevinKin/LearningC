53ab022808a6852d1cd180dfe3a709a76cbf7191
ArchDevinKin <DevinKin@163.com>
2017-08-21 20:32:43 +0800

    test13_4

---
+++ PointerOnC/Chapter13/test13_4.c
@@ -0,0 +1,67 @@
+/*************************************************************************
+    > File Name: test13_4.c
+    > Author: Elliot
+ ************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void
+swap(char *i, char *j, int elesize)
+{
+	char	x;
+	while (elesize--)
+	{
+		x = *i;
+		*i++ = *j;
+		*j++ = x;
+	}
+}
+
+int 
+comp(char *i, char *j)
+{
+	int		result = 0;
+	if (*i > *j)
+		result = 1;
+	else if( *i < *j) 
+		result = -1;
+	else
+		result = 0;
+
+	return result;
+}
+
+void
+sort(char *array, int elenum, int elesize, int (*compare)(char *, char *))
+{
+	register	char *i;
+	register	char *j;
+	register	char *last;
+	
+	last = array + (elenum - 1) * elesize;
+	for (i = array; i < last ; i += elesize)	
+		for (j = i + elesize; j <= last; j += elesize)
+			if (compare(i, j) > 0)
+				swap(i, j, elesize);
+
+}
+
+#define		SIZE	5
+
+int
+main(void)
+{
+	int		iarray[SIZE] = {99, 18, 98, 126, 48};
+	char	carray[SIZE] = {'B', 'M', 'O', 'C', 'A'};
+
+	for (int i = 0; i < SIZE; ++i)
+		printf("%d\n", iarray[i]);
+
+	sort((char *)iarray, (int)SIZE, (int)sizeof(int), comp);
+
+	printf("after sort: \n");
+	for (int i = 0; i < SIZE; ++i)
+		printf("%d\n", iarray[i]);
+	return	EXIT_SUCCESS;
+}
