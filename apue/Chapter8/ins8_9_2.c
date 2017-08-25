8b8246dc46f0459ec4a60264db768848bd83a5be
ArchDevinKin <DevinKin@163.com>
2017-08-25 08:54:45 +0800

    ins8_9

---
+++ apue/Chapter8/ins8_9_2.c
@@ -0,0 +1,49 @@
+/*************************************************************************
+    > File Name: ins8_9.c
+    > Author: Elliot
+ ************************************************************************/
+
+/*
+ * 程序输出两个字符串：一个由子进程输出，另一个由父进程输出。
+ * 因为输出依赖于内核使这两个进程运行的顺序及每个进程运行的时间，
+ * 所以该程序包含了一个竞争条件。
+ */
+
+#include "apue.h"
+
+static void charactatime(char *);
+
+int
+main(void)
+{
+	pid_t	pid;
+
+	TELL_WAIT();
+
+	if ((pid = fork()) < 0)
+		err_sys("fork error");
+	else if (pid == 0)
+	{
+		charactatime("output from child\n");
+		WAIT_PARENT();
+	}
+	else
+	{
+		charactatime("output from parent\n");
+		TELL_CHILD(pid);
+	}
+
+	exit(0);
+}
+
+static void
+charactatime(char *str)
+{
+	char	*ptr;
+	int		c;
+
+	setbuf(stdout, NULL);				/*  set unbuffered  */
+
+	for(ptr = str; (c = *ptr++) != 0;)
+		putc(c, stdout);
+}
