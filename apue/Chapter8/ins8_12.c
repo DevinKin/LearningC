5cd82a3262022b585ac2591b80dae57b75c18280
ArchDevinKin <DevinKin@163.com>
2017-08-25 15:06:06 +0800

    ins8_12

---
+++ apue/Chapter8/ins8_12.c
@@ -0,0 +1,24 @@
+/*************************************************************************
+    > File Name: ins8_12.c
+    > Author: Elliot
+ ************************************************************************/
+
+#include "apue.h"
+#include <sys/wait.h>
+
+int
+main(void)
+{
+	pid_t	pid;
+	
+	if ((pid = fork()) < 0)
+		err_sys("fork error");
+	else if (pid == 0)
+		if (execl("/home/king/bin/testinterp",
+					"testinterp", "myarg1", "MY ARG2", (char *)0) < 0)
+			err_sys("execl error");
+
+	if (waitpid(pid, NULL, 0) < 0)			/*  parent  */
+		err_sys("waitpid error");
+	exit(0);
+}
