965101f81604f28455dd9a07d55fd449153d6aea
ArchDevinKin <DevinKin@163.com>
2017-08-26 21:20:26 +0800

    test8_7

--- apue/Chapter8/test8_7.c
+++ apue/Chapter8/test8_7.c
@@ -23,15 +23,29 @@ main(void)
 	if ((fd = dirfd(dir)) < 0)		/*  get fd  */
 		err_sys("get dir fd error");
 
-	sprintf(buf, "%d\0", fd);
+	//sprintf(buf, "%d\0", fd);
 
 	if ((fd_flags = fcntl(fd, F_GETFD, 0)) < 0)
 		err_sys("get fd flags error");
+
+	/*
+	 * prove: close the flags of FD_CLOEXEC
+	 */
+
 
 	if (fd_flags & FD_CLOEXEC)
 		printf("close-on-exec is on\n");
 	else
 		printf("close-on-exec is off\n");
+
+	fd_flags &= ~FD_CLOEXEC;
+	/*
+	 * 关闭FD_CLOEXEC标志后，执行exec函数，关闭打开的目录流，但不会关闭与该流相关的
+	 * 文件描述符。
+	 */
+
+	fcntl(fd, F_SETFD, fd_flags);
+	sprintf(buf, "%d\0", fd);
 
 	if ((pid = fork()) < 0)
 		err_sys("fork error");
