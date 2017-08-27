fa2368e84b35822aadf6eb406d5aa04f8e7fe138
ArchDevinKin <DevinKin@163.com>
2017-08-27 22:38:26 +0800

    ins8_6_2

--- apue/Chapter8/ins8_6_2.c
+++ apue/Chapter8/ins8_6_2.c
@@ -17,9 +17,9 @@ main(void)
 	pid_t	pid;
 	if ((pid = fork()) < 0)
 		err_sys("fork error");
-	else if (pid == 0)
+	else if (pid == 0)					/*  first child  */
 	{
-		if ((pid = fork()) < 0)				/*  first child  */
+		if ((pid = fork()) < 0)				
 			err_sys("fork error");
 		else if (pid > 0)
 			exit(0);						/*  parent from second fork == first child  */
