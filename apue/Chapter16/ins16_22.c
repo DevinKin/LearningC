9eb60564e8fadb910ce82d4ff8e1f9dd32925539
DevinKin <DevinKin@163.com>
2018-01-01 23:15:04 +0800

    ins16_22

---
+++ apue/Chapter16/ins16_22.c
@@ -0,0 +1,35 @@
+/*************************************************************************
+    > File Name: ins16_22.c
+    > Author: King
+    > Mail: arturiapendragon_1@163.com 
+    > Created Time: 2018年01月01日 星期一 19时15分44秒
+ ************************************************************************/
+
+#include "apue.h"
+#include <errno.h>
+#include <sys/socket.h>
+
+int
+initserver(int type, const struct sockaddr *addr, socklen_t alen, int qlen)
+{
+	int fd, err;
+	int reuse=1;
+
+	if ((fd = socket(addr->sa_family, type, 0)) < 0)
+		return(-1);
+	if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0)
+		goto errout;
+	if (bind(fd, addr, alen) < 0)
+		goto errout;
+	if (type == SOCK_STREAM || type == SOCK_SEQPACKET)
+		if (listen(fd, qlen) < 0)
+			goto errout;
+	
+errout:
+	err = errno;
+	close(fd);
+	errno = err;
+	return(-1);
+
+
+}
