6de6a6bc976a38287226781e2f9422245bffb633
DevinKin <DevinKin@163.com>
2018-02-09 15:05:19 +0800

    udpcli02

---
+++ unp/Chapter8/udpcliserv/dgcliaddr.c
@@ -0,0 +1,34 @@
+/*************************************************************************
+    > File Name: dgcliaddr.c
+    > Author: King
+    > Mail: arturiapendragon_1@163.com 
+    > Created Time: 2018年02月09日 星期五 12时30分14秒
+ ************************************************************************/
+
+#include "unp.h"
+
+void
+dg_cli(FILE *fp, int sockfd, const SA* pservaddr, socklen_t servlen)
+{
+	int		n;
+	char	sendline[MAXLINE], recvline[MAXLINE + 1];
+	socklen_t len;
+	struct sockaddr *preply_addr;
+
+	preply_addr = Malloc(servlen);
+
+	while(Fgets(sendline, MAXLINE, fp) != NULL) {
+		Sendto(sockfd, sendline, strlen(sendline), 0, pservaddr, servlen);
+
+		len = servlen;
+		n = Recvfrom(sockfd, recvline, MAXLINE, 0, preply_addr, &len);
+		if (len != servlen || memcmp(pservaddr, preply_addr, len) != 0)
+		{
+			printf("reply from %s (ignored)\n", Sock_ntop(preply_addr, len));
+			continue;
+		}
+		recvline[n] = 0;		/* null terminate */
+		Fputs(recvline, stdout);
+	}
+
+}
---
+++ unp/Chapter8/udpcliserv/udpcli02.c
@@ -0,0 +1,28 @@
+/*************************************************************************
+    > File Name: udpcli01.c
+    > Author: King
+    > Mail: arturiapendragon_1@163.com 
+    > Created Time: 2018年02月09日 星期五 00时20分23秒
+ ************************************************************************/
+
+#include "unp.h"
+
+int
+main(int argc, char **argv)
+{
+	int		sockfd;
+	struct sockaddr_in	servaddr;
+
+	if (argc != 2)
+		err_quit("usage: udpcli <IPaddress>");
+
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(7);
+	Inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
+
+	sockfd = Socket(AF_INET, SOCK_DGRAM, 0);
+
+	dg_cli(stdin, sockfd, (SA *)&servaddr, sizeof(servaddr));
+	exit(0);
+}
